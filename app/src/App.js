488128265c1663c0d10d9dbe2afc6485545ea563
smcconaha <steve.mcconaha@student.awesomeinc.org>
2022-10-19 19:33:44 +0000

    Added basic structure with App.js, index.js, and created css file.

---
+++ app/src/App.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './css/styles.css'
+
+function App() {
+    const [data, setData] = useState([]);
+    const [page, setPage] = useState('Home')
+    return(
+        <div className='container-fluid'>
+
+        </div>
+    )
+}
+export default App
