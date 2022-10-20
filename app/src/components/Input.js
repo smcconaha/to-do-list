733ac291b8536912d0e3de0c203eb09190494b32
smcconaha <steve.mcconaha@student.awesomeinc.org>
2022-10-20 22:40:28 +0000

    Added divs and rows to todo list

--- app/src/components/Input.js
+++ app/src/components/Input.js
@@ -5,50 +5,64 @@ let nextId = 0;
 export default function Input(props) {
   const [title, setTitle] = useState(''); //used to set the actual toDo title
   const [todos, setTodos] = useState([]); //used to set the todo list
-  
+
   return (
-    <div>
-      <h1>To-Do List</h1>
-      <div className="input-group mb-3">
-        <input
-          className="form-control"
-          type="text"
-          placeholder="Enter To-do..." 
-          value={title} //feeds into useState to set individual todo title
-          onChange={e => setTitle(e.target.value)} //once changed and button clicked, todo recorded
+    <div className="row">
+      <div className="col-10">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="col-10">
+        <div className="input-group mb-3">
+          <input
+            className="form-control"
+            type="text"
+            placeholder="Enter To-do..."
+            value={title} //feeds into useState to set individual todo title
+            onChange={e => setTitle(e.target.value)} //once changed and button clicked, todo recorded
           />
-        <button
-          className="btn btn-outline-secondary"
-          type="button"
-          onClick={() => {
-            setTitle('');
-            setTodos([
-            ...todos, //old items are retained with each addition, spread op
-            { id: nextId++, title: title } //increment ID with each addition
-          ]);
-        }}>Enter</button>
-      </div>
-      <div>
-        <ul>
-          {todos.map(todo => (
-            <li key={todo.id}>{todo.title}
-            <button
-              className="btn btn-success"
-              type="button"
-              onClick={() => {
-                console.log(todos);
-                const nextTodos = [...todos];
-                nextTodos.splice(todos.indexOf(todo), 1);
-                setTodos(nextTodos);
-            }}>
-              Remove
-            </button>  
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div>
-      </div>
+          <button
+            className="btn btn-outline-secondary"
+            type="button"
+            onClick={() => {
+              setTitle('');
+              setTodos([
+                ...todos, //old items are retained with each addition, spread op
+                { id: nextId++, title: title } //increment ID with each addition
+              ]);
+            }}>Enter</button>
+        </div>
+        <div>
+          <ul>
+            {todos.map(todo => (
+              <div>
+                <div className="form-check">
+                  <input 
+                    className="form-check-input" 
+                    type="checkbox" 
+                    value="" 
+                    id="flexCheckDefault" />
+                  <label 
+                    className="form-check-label" 
+                    for="flexCheckDefault">
+                    Completed
+                  </label>
+                </div>
+                <li key={todo.id}>{todo.title}
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => {
+                      const nextTodos = [...todos];
+                      nextTodos.splice(todos.indexOf(todo), 1);
+                      setTodos(nextTodos);
+                    }}>
+                    Remove
+                  </button>
+                </li>
+              </div>
+            ))}
+          </ul>
+        </div>
+      </div>  
     </div>
   );
 }
